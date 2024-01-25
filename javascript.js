be0362f535d851f3745e796e47a26b3c3ef05bd6
Samira Amiri <87242121+Samira-Amiri@users.noreply.github.com>
2024-01-25 11:12:07 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,123 @@
+const grid = document.querySelector("#grid");
+
+let size = 16;
+let color = "black";
+
+function makeGrid(size) {
+  for (let i = 0; i < size; i++) {
+    let divColumn = document.createElement("div");
+    divColumn.setAttribute("style", "margin: 0; padding: 0; display:flex; flex: 1;");
+    grid.appendChild(divColumn);
+
+    for (let j = 0; j < size; j++) {
+      let divRow = document.createElement("div");
+      divRow.setAttribute("style", "border: solid 1px darkgrey; margin: 0; padding: 0; flex: 1;");
+      divColumn.appendChild(divRow);
+      divRow.classList.add("hover");
+    }
+  }
+
+  let canDraw = false;
+
+  window.addEventListener("mouseup", function () {
+    canDraw = false;
+  });
+
+  let hoverElements = document.querySelectorAll(".hover");
+
+  hoverElements.forEach(function (hoverElement) {
+    hoverElement.addEventListener("mouseover", function () {
+      if (canDraw == true) {
+        colorElement(hoverElement);
+      }
+    });
+    hoverElement.addEventListener("mousedown", function () {
+      canDraw = true;
+      colorElement(hoverElement);
+    });
+  });
+}
+
+makeGrid(size);
+
+function colorElement(hoverElement) {
+  if (color == "black") {
+    hoverElement.style.backgroundColor = "black";
+  } else if (color == "random") {
+    hoverElement.style.backgroundColor = oneRandomColor();
+  } else if (color == "colorful") {
+    hoverElement.style.backgroundColor = randomColor();
+  } else if (color == "colorPicker") {
+    hoverElement.style.backgroundColor = document.getElementById("color-picker").value;
+  } else {
+    hoverElement.style.backgroundColor = "white";
+  }
+}
+
+const gridSize = document.querySelector(".btnSize");
+
+gridSize.addEventListener("click", function () {
+  let sizeChoice = prompt("Choose grid size (1-100)");
+
+  while (sizeChoice < 1 || sizeChoice > 100 || sizeChoice % 1 != 0) {
+    sizeChoice = prompt("The number must be an integer between 1 and 100");
+  }
+
+  size = sizeChoice;
+  grid.replaceChildren();
+  makeGrid(sizeChoice);
+});
+
+const eraser = document.querySelector(".btnEraser");
+
+eraser.addEventListener("click", function () {
+  color = "white";
+});
+
+const reset = document.querySelector(".btnReset");
+
+reset.addEventListener("click", function () {
+  grid.replaceChildren();
+  makeGrid(size);
+});
+
+const btnBlack = document.querySelector(".btnBlack");
+const btnColorful = document.querySelector(".btnColorful");
+const btnRandom = document.querySelector(".btnRandom");
+const colorPickerButton = document.querySelector(".colorp");
+const colorPickerInput = document.querySelector("#color-picker");
+
+btnBlack.addEventListener("click", function () {
+  color = "black";
+});
+
+btnRandom.addEventListener("click", function () {
+  color = "random";
+});
+
+btnColorful.addEventListener("click", function () {
+  color = "colorful";
+});
+
+colorPickerButton.addEventListener("click", function () {
+  color = "colorPicker";
+});
+
+colorPickerInput.addEventListener("input", function () {
+  if (color === "colorPicker") {
+    color = colorPickerInput.value;
+  }
+});
+
+function randomColor() {
+  const hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+  let colorCode = "";
+  for (let i = 0; i < 6; i++) {
+    colorCode += hexArray[Math.floor(Math.random() * 16)];
+  }
+  return `#${colorCode}`;
+}
+
+function oneRandomColor() {
+  return randomColor();
+}
